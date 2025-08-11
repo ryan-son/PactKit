53f8d688fb9c0258c07a3d376efcf9de9a4cf587
Geonhee Son <ryan.son1002@gmail.com>
2025-08-11 23:24:47 +0900

    chore: Initialize Swift package for PactKit

---
+++ Package.swift
@@ -0,0 +1,37 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+  name: "PactKit",
+  platforms: [
+    .iOS(.v13),
+    .macCatalyst(.v15),
+    .macOS(.v10_15),
+    .tvOS(.v15),
+    .visionOS(.v1),
+    .watchOS(.v8),
+  ],
+  products: [
+    .library(name: "PactKit", targets: ["PactKit"]),
+  ],
+  targets: [
+    .target(name: "PactKit"),
+    .testTarget(
+      name: "PactKitTests",
+      dependencies: ["PactKit"]
+    ),
+  ]
+)
+
+#if compiler(>=6)
+for target in package.targets where target.type != .system && target.type != .test {
+  target.swiftSettings = target.swiftSettings ?? []
+  target.swiftSettings?.append(contentsOf: [
+    .enableExperimentalFeature("StrictConcurrency"),
+    .enableUpcomingFeature("ExistentialAny"),
+    .enableUpcomingFeature("InferSendableFromCaptures"),
+  ])
+}
+#endif
---
+++ Sources/PactKit/PactKit.swift
@@ -0,0 +1,14 @@
+//
+//  PactKit.swift
+//  PactKit
+//
+//  Created by Geonhee on 8/11/25.
+//
+
+import Foundation
+
+public enum Pact {
+  public static func hello() {
+    print("Welcome to PactKit!")
+  }
+}
